63ae69c2ee4875c3f3c288a9917c646ddf5a61ec
Sanaroom <167286039+Sanaroom@users.noreply.github.com>
2024-05-09 15:35:10 +0900

    deviseのビュー作成、ユーザー管理機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_09_051706) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_09_060110) do
+  create_table "items", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", default: "", null: false
+    t.integer "category_id", null: false
+    t.integer "item_statue_id", null: false
+    t.integer "shipping_cost_id", null: false
+    t.integer "prefecture_id", null: false
+    t.text "description", null: false
+    t.integer "shipping_date_id", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
@@ -29,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_09_051706) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
