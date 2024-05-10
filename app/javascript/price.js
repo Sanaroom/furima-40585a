e44feabe933ed707dd64686e16161b9c83d70ad2
Sanaroom <167286039+Sanaroom@users.noreply.github.com>
2024-05-10 14:48:34 +0900

    javaが読み込まれなかったのは？謎

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,11 +1,10 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price"); // 価格入力フィールドのID
+  
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-
     const addTaxDom = document.getElementById("add-tax-price"); // 手数料表示要素のID
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-
     const profitNumber = document.getElementById("profit") // 利益表示要素のID
     profitNumber.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
   })
