2973f79c3abee5c314238878915150d5eb3ff5c1
Sanaroom <167286039+Sanaroom@users.noreply.github.com>
2024-05-10 09:35:32 +0900

    出品ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,21 @@
 class ItemsController < ApplicationController
   
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item=Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:user, :name, :category_id, :item_statue_id, :shipping_cost_id, :prefecture_id, :description, :shipping_date_id, :price) # フォームから送られてくるparamsのうち、DBに保存したい値のキーのみをここに記入
+  end
 end
+
