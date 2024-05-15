f35334c364b6e487080edca4a28999970514b925
Sanaroom <167286039+Sanaroom@users.noreply.github.com>
2024-05-15 22:26:35 +0900

    javaの訂正、認証

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,11 +1,18 @@
-window.addEventListener('turbo:load', () => {
+const price = () => {
+
   const priceInput = document.getElementById("item-price");
-  
-  priceInput.addEventListener("input", () => {
-    const inputValue = priceInput.value;// 
+  priceInput.addEventListener("input", function(){
+    
+    const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
     const profitNumber = document.getElementById("profit") 
     profitNumber.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
-  })
-});
+
+
+  });
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
+
