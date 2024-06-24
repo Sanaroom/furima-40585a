68ab30b331e06c4cb208501fde7df9339711d7fb
Sanaroom <167286039+Sanaroom@users.noreply.github.com>
2024-06-24 10:43:56 +0900

    使用不可

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users, only: [:index,:show] do
+  resources :users, only: [:show] do
     collection do
       get :favorites
     end
