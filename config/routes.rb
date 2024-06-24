bb89d683d045a332a1d57f808b3a4bfacc7b1a0f
Sanaroom <167286039+Sanaroom@users.noreply.github.com>
2024-06-24 10:23:58 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   
   resources :items do
     resources :orders , only: [:index,:create]
-    resources :comments, only: [:create, :destroy] 
+    resources :comments, only: [:create] 
     collection do
       get 'search'
     end
